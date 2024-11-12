13ace6dc18a57f82c6d9ce4f93ae9ffdfd023013
Hrushikesh Pathak <hrushikeshpathak@Hrushikeshs-MacBook-Pro.local>
2024-11-12 20:34:54 +0530

    last commit 3

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,10 +6,10 @@ const About = () => {
         <div className='marginal'>
             <div className='md:flex md:justify-between md:items-center'>
                 <div className='md:w-[59%]'>
-                    <h1 className='md:text-5xl text-4xl font-semibold text-[#00565C] leading-normal'>Sustainable development</h1>
-                    <h1 className='md:text-5xl text-4xl font-semibold text-[#161616] leading-normal'>can only be realised through <br />
+                    <h1 className='md:text-4xl text-4xl font-semibold text-[#00565C]'>Sustainable development</h1>
+                    <h1 className='md:text-4xl text-4xl font-semibold text-[#161616]'>can only be realised through 
                         the widespread adoption of </h1>
-                    <h1 className='md:text-5xl text-4xl font-semibold text-[#00565C] leading-normal'>renewable energy</h1>
+                    <h1 className='md:text-4xl text-4xl font-semibold text-[#00565C]'>renewable energy</h1>
                     <p className='mt-7 text-xl'>
                         At Sembcorp, we realise the immense potential of renewable energy to advance economic and infrastructure development, improve energy security and mitigate climate change.
                     </p>
@@ -28,7 +28,7 @@ const About = () => {
                     </div>
                 </div>
                 <div className='md:w-[39%] md:mt-[25%]'>
-                    <h1 className='md:text-5xl text-4xl font-semibold text-[#161616] leading-normal'>Catalysing <span className='text-[#00565C]'>energy transition</span></h1>
+                    <h1 className='md:text-4xl text-4xl font-semibold text-[#161616]'>Catalysing <span className='text-[#00565C]'>energy transition</span></h1>
                     
                     <p className='mt-7 text-xl'>
                         At Sembcorp, we realise the immense potential of renewable energy to advance economic and infrastructure development, improve energy security and mitigate climate change.
